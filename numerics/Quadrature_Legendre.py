b028c363ca57d622df8e70c345eb57bc3ba3b520
Daniel Brue <danielbrue@gmail.com>
2019-11-08 10:04:58 -0600

    still working on quadrature weight calculation formula

--- numerics/Quadrature_Legendre.py
+++ numerics/Quadrature_Legendre.py
@@ -1,36 +1,38 @@
 #!/usr/bin/env python3
 import numpy
+import math
 import scipy.special as spsp
 import scipy.linalg as spla
 import numpy.linalg as npla
 
 if (__name__ == '__main__'):
 
-    N = 5
+    N = 4
 
     PN=spsp.legendre(N)
     print(PN.roots)
     print(type(PN.roots))
-    print(numpy.sort(PN.roots))
+    print('ABSCISSAS',numpy.sort(PN.roots))
 
     X = numpy.sort(PN.roots)
     M = numpy.zeros([N,N],dtype=numpy.float64)
-    Msq = numpy.zeros([N,N],dtype=numpy.float64)
     for i in range(N):
         P = spsp.legendre(i)
         p = numpy.zeros([N])
         for j in range(N):
-            M[i,j] = P(X[j])
-            Msq[i,j] = (P(X[j]))**2
+            M[i,j] = P(X[j]) #/(math.sqrt(2/(2*i+1)))
 
     M2=numpy.matmul(M,M.transpose())
-    W,vr=spla.eig(M)
-    print(W)
-    W2,vr=spla.eig(M2)
-    print(W2)
-
-    Ones = numpy.ones(N)
-    w = npla.solve(Msq,Ones)
-    print(w)
+    #W,vr=spla.eig(M)
+    print(M2)
+    W2,vr=spla.eigh(M2)
+    print()
+    print('EIGVALS',W2)
+    print()
+    print('EIGVECS')
+    print(vr)
+    print()
+    print('ORTHOGONALITY CHECK')
+    print(numpy.matmul(vr,vr.transpose()))
 
 
