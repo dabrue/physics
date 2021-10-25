b7e853f59017ba066b54ac9e0da98fe3e6c47f05
Daniel Brue <danielbrue gmail>
2021-10-24 21:54:46 -0500

    save

--- Numerics/Numerov/numerov.py
+++ Numerics/Numerov/numerov.py
@@ -10,7 +10,7 @@ import numpy as np
 import logging
 
 
-def plotSolution(X,V,Fl,Fr):
+def plotSolution(X,V,Fs,Fl,Fr,T):
     import matplotlib.pyplot as plt
 
     fig0 = plt.figure()
@@ -20,13 +20,16 @@ def plotSolution(X,V,Fl,Fr):
     plt.plot(X,V,'k-')
 
     if (type(Fl) == list ):
-        for i in range(len(Fl)):
-            plt.plot(X,Fl[i],'b-',label='Left Sol.')
-        for i in range(len(Fr)):
-            plt.plot(X,Fr[i],'r-',label='Right Sol.')
+        for i in range(len(Fs)):
+            plt.plot(X,Fs[i],'k-',label='Solution')
+        #for i in range(len(Fl)):
+        #    plt.plot(X,Fl[i],'b-',label='Left Sol.')
+        #for i in range(len(Fr)):
+        #    plt.plot(X,Fr[i],'r-',label='Right Sol.')
     else:
         plt.plot(X,Fl,'b-',label='Left Sol.')
         plt.plot(X,Fr,'r-',label='Right Sol.')
+    plt.ylim([-math.pi,3*math.pi])
     plt.show()
 
 def matchLR(X,Fl,Fr,matchi = None):
@@ -51,17 +54,14 @@ def matchLR(X,Fl,Fr,matchi = None):
     ml = tl[2]-tl[0]
     mr = tr[2]-tr[0]
     mdiff = ml-mr
-    mrat = ml/mr - 1
+    mrat = abs(ml/mr) - 1
+    #mrat = (ml/mr) - 1
     mdet = (tr[0]*tl[2]-tr[2]*tl[0])
 
     return mdiff, mrat, mdet
-
-
-
-
     
 
-def int1dLR(X, V, E):
+def int1dLR(X, V, E, matchi=None):
     '''
 
     Integrate in 1D for a given function V such that the solution function
@@ -89,28 +89,54 @@ def int1dLR(X, V, E):
     T = np.zeros_like(X)
     T = E - V
 
+    if (not matchi):
+        matchi = int(N/2)
 
     if (T[0] > 0.0 or T[-1] > 0.0):
         log.warning('WARNING: Numerov 1D, end-points do not lead to local solution')
+        print(T[0],T[-1],E,V[0],V[-1])
+        exit()
+
+    classical_bound_index_L = 0
+    for i in range(N):
+        if (T[i] > 0.0):
+            classical_bound_index_L = i
+            break
+    classical_bound_index_R = 0
+    for i in range(N-1,-1,-1):
+        if (T[i] > 0.0):
+            classical_bound_index_R = i
+            break
+
+    #if (matchi < classical_bound_index_L or matchi > classical_bound_index_R):
+    #    log.error('Match Point not in classical region')
+    #    exit()
 
     Fl = np.zeros_like(X)
     Fr = np.zeros_like(X)
 
     # Left-to-Right propagation
     Fl[1] = initVal
-    for i in range(2,N):
+    for i in range(2,classical_bound_index_R):
         Fl[i] = ( Fl[i-1]*(2+h**2*T[i-1]*5/6)- Fl[i-2]*(1-h**2*T[i-2]/12))/(1-T[i]*h**2/12)
         if (abs(Fl[i]) > 1.0e9):
             Fl *= 1/1.0e8
 
     # Right-to-Left propagation
     Fr[-2] = initVal
-    for i in range(N-3,-1,-1):
+    for i in range(N-3,classical_bound_index_L,-1):
         Fr[i] = ( Fr[i+1]*(2+h**2*T[i+1]*5/6)- Fr[i+2]*(1-h**2*T[i+2]/12))/(1-T[i]*h**2/12)
         if (abs(Fr[i]) > 1.0e9):
             Fr *= 1/1.0e8
 
-    return Fl,Fr
+    F = np.zeros_like(X)
+    F[:matchi] = Fl[:matchi]
+    F[matchi:] = Fr[matchi:]
+    norm = math.sqrt(np.sum(F**2) * (X[1]-X[0]))
+    if (norm > 0):
+        F *= 1/norm
+
+    return F,Fl,Fr,T
 
 
 ####################################################################################################
@@ -121,44 +147,49 @@ def TEST0():
     # Solve harmonic potential, V = (1/2)*k*x^2
 
     nPts = 1001
+
+    matchi = int(nPts/2)
     
-    k = 1.0
+    k = 10.0
 
-    xMin = -math.pi
-    xMax =  math.pi
+    xMin = -10*math.pi
+    xMax =  10*math.pi
 
-    Emin = 0.0
-    Emax = 1000.0
-    Ens = np.linspace(Emin,Emax,101)
+    nE = 901
+    Emin = 1.0
+    Emax = 10.0
+    Ens = np.linspace(Emin,Emax,nE)
 
     X = np.linspace(xMin,xMax,nPts)
     V = k*X**2/2
 
+    Fs = []
     Fls = []
     Frs = []
     dets = []
     rats = []
     difs = []
-    for i in range(len(Ens)):
+    for i in range(nE):
         E = Ens[i]
         print('Energy ',E)
-        Fl,Fr = int1dLR(X,V,E)
+        F,Fl,Fr,T = int1dLR(X,V,E)
+        Fs.append(F)
         Fls.append(Fl)
         Frs.append(Fr)
-        mdif,mrat,mdet = matchLR(X,Fl,Fr)
+        mdif,mrat,mdet = matchLR(X,Fl,Fr,matchi=matchi)
         dets.append(mdet)
         difs.append(mdif)
         rats.append(mrat)
 
 
-    rtn = plotSolution(X,V,Fls,Frs)
+    #rtn = plotSolution(X,V,Fs,Fls,Frs,T)
 
     fig0=plt.figure()
     ax0 = fig0.add_subplot(1,1,1)
-    plt.plot(Ens,difs,'r-',label='Differences')
-    plt.plot(Ens,rats,'g--',label='Ratios')
-    plt.plot(Ens,dets,'b.-',label='Determinant')
-    plt.ylim([-10,10])
+    plt.plot(Ens,difs,'r*',label='Differences')
+    plt.plot(Ens,rats,'g.',label='Ratios')
+    plt.plot(Ens,dets,'b.',label='Determinant')
+    #plt.ylim([-10,10])
     plt.show()
 
     return 0
